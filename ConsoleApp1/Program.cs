67837c0bc2bb4c6172a67b488851c7927be1d991
李健宁 <lijianning@flytexpress.com>
2020-04-03 17:36:51 +0800

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using SciterSharp;
+using SciterSharp.Interop;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            PInvokeWindows.OleInitialize(IntPtr.Zero);
+            var window = new SciterWindow();
+
+            window.CreateMainWindow(1500, 800);
+            window.CenterTopLevelWindow();
+            window.Title = "Test";
+
+            var host = new SciterHost();
+            host.SetupWindow(window);
+            window.Show();
+
+            PInvokeUtils.RunMsgLoop();
+        }
+    }
+}
