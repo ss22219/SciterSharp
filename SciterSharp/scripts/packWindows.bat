4e10bb5c5db5d6700a921fa352e39ae743383940
ss22219 <ss22219@qq.com>
2020-04-04 03:23:35 +0800

    ...

--- SciterSharp/scripts/packWindows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo ######## Packing '../LibConsole' directory to 'ArchiveResource.cs' ########
-cd %~dp0
-packfolder.exe ../LibConsole ../ArchiveResource.cs -csharp
-fart.exe -- ..\ArchiveResource.* SciterAppResource SciterSharp
-echo OK
